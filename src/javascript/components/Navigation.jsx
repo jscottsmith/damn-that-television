ab620410600c6e39f1dfcd1ec2817c83c75cc4c5
J Scott Smith <jscsmith@gmail.com>
2016-04-03 12:50:33 -0700

    github link

--- src/javascript/components/Navigation.jsx
+++ src/javascript/components/Navigation.jsx
@@ -9,7 +9,7 @@ export default class Navigation extends React.Component {
                 <IndexLink to="/" className="logo" activeClassName="is-current">Home</IndexLink>
                 <Link to="/projects" activeClassName="is-current">Projects</Link>
                 <Link to="/makejavascriptgreatagain" activeClassName="is-current">Outside</Link>
-                <Link to="https://github.com/MadeInHaus/react-flux-gulp-starter" target="_blank">Github</Link>
+                <Link to="https://github.com/jscottsmith" target="_blank">Github</Link>
             </div>
         );
     }
