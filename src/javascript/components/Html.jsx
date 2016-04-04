cc1c272bc93c04f1fc0318c6f3e3e2049b329eca
J Scott Smith <jscsmith@gmail.com>
2016-04-03 18:58:21 -0700

    favicon update

--- src/javascript/components/Html.jsx
+++ src/javascript/components/Html.jsx
@@ -26,7 +26,7 @@ class Html extends React.Component {
                     <meta property="og:url" content={this.context.siteUrl()} />
                     <meta property="og:image" content={this.context.assetUrl('/image.jpg')} />
                     <meta property="og:description" content="Description Here" />
-                    <link rel="shortcut icon" href={this.context.assetUrl('favicon.ico')} />
+                    <link rel="shortcut icon" href={this.context.assetUrl('/images/favicon.png')} />
                     <link rel="stylesheet" href={this.context.assetUrl('/css/styles.css')} />
                     <script src="https://use.typekit.net/bwo5nqc.js"></script>
                     <script dangerouslySetInnerHTML={{ __html: 'try{Typekit.load({ async: true });}catch(e){}'}}/>
