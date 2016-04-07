291a6409f2b2c5ca35968fdbbb3c5fe035b15e03
J Scott Smith <jscsmith@gmail.com>
2016-04-07 11:03:25 -0700

    add `Index` component for the index route

---
+++ src/javascript/components/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Canvas from './Canvas';
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <article className="page-index">
+                <Canvas />
+            </article>
+        );
+    }
+}
--- src/javascript/components/Routes.jsx
+++ src/javascript/components/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Application from 'components/Application';
+import Index from 'components/Index';
 import Project from 'components/Project';
 import Projects from 'components/Projects';
 import NotFound from 'components/NotFound';
 
 export default (
     <Route path="/" component={Application}>
-        <IndexRoute component={Projects}/>
+        <IndexRoute component={Index}/>
         <Route path="projects" component={Projects}/>
         <Route path="projects/:slug" component={Project}/>
         <Route path="*" component={NotFound}/>
