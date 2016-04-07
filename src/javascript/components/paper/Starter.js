54691adc87445ebdf59157ab69fe8c51b0e1b7d3
J Scott Smith <jscsmith@gmail.com>
2016-04-07 11:11:48 -0700

    add `Starter` class for setting up a Paper view with the basics

---
+++ src/javascript/components/paper/Starter.js
@@ -0,0 +1,49 @@
+import { Paper, Path, Point, Size, Shape } from './Paper';
+
+export default class Waves {
+    constructor(canvas) {
+        this.paper = Paper;
+        this.canvas = canvas;
+        Paper.setup(canvas);
+    }
+
+    init() {
+        this.setSize();
+        this.drawBg();
+
+        Paper.view.draw();
+
+        Paper.view.onResize = (event) => {
+            this.drawBg();
+            this.init();
+        };
+
+        // Draw frames
+        this.render();
+    }
+
+    drawBg() {
+        const c = Paper.view.center;
+        const topLeft = c.subtract(c.multiply(2));
+        const bottomRight = c.add(c);
+        const shape = new Shape.Rectangle(topLeft, bottomRight);
+        shape.fillColor = 'white';
+    }
+
+    setSize() {
+        // set the size of the canvas/view to the window
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+
+        this.canvas.width = w;
+        this.canvas.height = h;
+
+        Paper.view.viewSize = new Size(w, h);
+    }
+
+    render() {
+        Paper.view.onFrame = (event) => {
+            // Animate things
+        };
+    }
+}
