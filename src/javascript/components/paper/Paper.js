00ea69a96670790059e729c54f98653920c7b781
J Scott Smith <jscsmith@gmail.com>
2016-04-12 11:03:59 -0700

    add `Rectangle` and `Group` to the Paper dependency model

--- src/javascript/components/paper/Paper.js
+++ src/javascript/components/paper/Paper.js
@@ -1,9 +1,11 @@
 // Paper and Paper class references
 let Paper = null;
+let Group = null;
 let Size = null;
 let Path = null;
 let Point = null;
 let Shape = null;
+let Rectangle = null;
 
 // Wait for the window to be read before importing paper
 if (typeof window !== 'undefined') {
@@ -11,10 +13,12 @@ if (typeof window !== 'undefined') {
     Paper = require('paper/dist/paper-core');
 
     // Aliases
+    Group = Paper.Group;
     Size = Paper.Size;
     Path = Paper.Path;
     Point = Paper.Point;
     Shape = Paper.Shape;
+    Rectangle = Paper.Rectangle;
 }
 
-export { Paper, Path, Point, Size, Shape };
+export { Paper, Group, Path, Point, Size, Shape, Rectangle };
