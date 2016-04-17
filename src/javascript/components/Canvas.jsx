d84f1d2160fcb78f9d0831349499095e7528a616
J Scott Smith <jscsmith@gmail.com>
2016-04-17 00:54:17 -0700

    update `Canvas` to call teardown when unmounting

--- src/javascript/components/Canvas.jsx
+++ src/javascript/components/Canvas.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import Diamonds from './paper/Diamonds';
+import Starter from './paper/Starter';
 
 export default class Paper extends React.Component {
     componentDidMount() {
         this.setupPaperCanvas();
     }
 
+    componentWillUnmount() {
+        this.paper.teardown();
+    }
+
     setupPaperCanvas() {
         const canvas = this.refs.paperCanvas;
         // add resize attribute
         canvas.setAttribute('resize', 'true');
-        canvas.setAttribute('hidpi', 'true');
 
-        const diamonds = new Diamonds(canvas);
-        diamonds.init();
+        this.paper = new Starter(canvas);
+        this.paper.init();
     }
 
     render() {
