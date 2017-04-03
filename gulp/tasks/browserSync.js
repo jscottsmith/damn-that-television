7a264e4b3770c27a6485cee6343e25f712db6033
J Scott Smith <jscsmith@gmail.com>
2017-04-02 22:24:35 -0700

    remove gulp

--- gulp/tasks/browserSync.js
+++
@@ -1,7 +0,0 @@
-var browserSync = require('browser-sync');
-var gulp = require('gulp-help')(require('gulp'));
-var config = require('../config').browserSync;
-
-gulp.task('browserSync', false, ['server'], function() {
-    browserSync(config);
-});
--- gulp/tasks/production.js
+++
@@ -1,3 +0,0 @@
-var gulp = require('gulp-help')(require('gulp'));
-
-gulp.task('production', 'Build static assets and compress for production', ['staticAssets', 'images', 'minifyCss', 'uglifyJs']);
