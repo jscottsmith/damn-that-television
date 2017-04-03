d70383eb8f72385977bd858a2f89c9247cdb6eed
J Scott Smith <jscsmith@gmail.com>
2017-04-02 22:23:29 -0700

    remove markdown and use js modules

---
+++ src/content/projects/david-and-goliath.js
@@ -0,0 +1,13 @@
+const project = {
+    title: 'David & Goliath',
+    slug: 'david-and-goliath',
+    role: 'Front-end Developer',
+    url: 'http://www.dng.com',
+    description: 'A fresh new site for David&Goliath to showcase their award winning creative work.',
+    colors: {
+        primary: '#e9167c',
+    },
+    content: 'David&Goliath Website',
+};
+
+export default project;
