6bceee87b67db3c615eea65cd3921bd9069e21c3
J Scott Smith <jscsmith@gmail.com>
2017-04-02 22:39:13 -0700

    autofix lint errors in responsive loader files

--- src/javascript/libs/responsive-loader/adapters/jimp.js
+++ src/javascript/libs/responsive-loader/adapters/jimp.js
@@ -1,36 +1,35 @@
-'use strict';
 
-var jimp = require('jimp');
+const jimp = require('jimp');
 
-module.exports = function (imagePath) {
-  var readImage = jimp.read(imagePath);
+module.exports = function(imagePath) {
+    const readImage = jimp.read(imagePath);
 
-  return {
-    metadata: function metadata() {
-      return readImage.then(function (image) {
-        return { width: image.bitmap.width, height: image.bitmap.height };
-      });
-    },
-    resize: function resize(_ref) {
-      var width = _ref.width,
-          quality = _ref.quality,
-          background = _ref.background,
-          mime = _ref.mime;
-      return new Promise(function (resolve, reject) {
-        readImage.then(function (image) {
-          image.clone().resize(width, jimp.AUTO).quality(quality).background(parseInt(background, 16) || 0xFFFFFFFF).getBuffer(mime, function (err, data) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve({
-                data,
-                width,
-                height: this.bitmap.height
-              });
-            }
-          });
-        });
-      });
-    }
-  };
+    return {
+        metadata: function metadata() {
+            return readImage.then(function(image) {
+                return { width: image.bitmap.width, height: image.bitmap.height };
+            });
+        },
+        resize: function resize(_ref) {
+            let width = _ref.width,
+                quality = _ref.quality,
+                background = _ref.background,
+                mime = _ref.mime;
+            return new Promise(function(resolve, reject) {
+                readImage.then(function(image) {
+                    image.clone().resize(width, jimp.AUTO).quality(quality).background(parseInt(background, 16) || 0xFFFFFFFF).getBuffer(mime, function(err, data) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve({
+                                data,
+                                width,
+                                height: this.bitmap.height,
+                            });
+                        }
+                    });
+                });
+            });
+        },
+    };
 };
--- src/javascript/libs/responsive-loader/adapters/sharp.js
+++ src/javascript/libs/responsive-loader/adapters/sharp.js
@@ -1,46 +1,45 @@
-'use strict';
 
-var sharp = require('sharp');
+const sharp = require('sharp');
 
-module.exports = function (imagePath) {
-  var image = sharp(imagePath);
+module.exports = function(imagePath) {
+    const image = sharp(imagePath);
 
-  return {
-    metadata: function metadata() {
-      return image.metadata();
-    },
-    resize: function resize(_ref) {
-      var width = _ref.width,
-          quality = _ref.quality,
-          background = _ref.background,
-          mime = _ref.mime;
-      return new Promise(function (resolve, reject) {
-        var resized = image.clone().resize(width, null);
+    return {
+        metadata: function metadata() {
+            return image.metadata();
+        },
+        resize: function resize(_ref) {
+            let width = _ref.width,
+                quality = _ref.quality,
+                background = _ref.background,
+                mime = _ref.mime;
+            return new Promise(function(resolve, reject) {
+                let resized = image.clone().resize(width, null);
 
-        if (background) {
-          resized = resized.background(background).flatten();
-        }
+                if (background) {
+                    resized = resized.background(background).flatten();
+                }
 
-        if (mime === 'image/jpeg') {
-          resized = resized.jpeg({
-            quality
-          });
-        }
+                if (mime === 'image/jpeg') {
+                    resized = resized.jpeg({
+                        quality,
+                    });
+                }
 
-        resized.toBuffer(function (err, data, _ref2) {
-          var height = _ref2.height;
+                resized.toBuffer(function(err, data, _ref2) {
+                    const height = _ref2.height;
 
-          if (err) {
-            reject(err);
-          } else {
-            resolve({
-              data,
-              width,
-              height
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({
+                            data,
+                            width,
+                            height,
+                        });
+                    }
+                });
             });
-          }
-        });
-      });
-    }
-  };
+        },
+    };
 };
