789dc28095d02c50723ad1ab2619f0ba5efc8e76
J Scott Smith <jscsmith@gmail.com>
2017-06-29 23:58:06 -0700

    remove paper experiments

--- src/javascript/components/pages/Index.jsx
+++ src/javascript/components/pages/Index.jsx
@@ -1,12 +1,7 @@
 import React, { PureComponent } from 'react';
-import Canvas from 'components/Canvas';
 
 export default class Index extends PureComponent {
     render() {
-        return (
-            <article className="page-index">
-                <Canvas />
-            </article>
-        );
+        return <article className="page-index" />;
     }
 }
