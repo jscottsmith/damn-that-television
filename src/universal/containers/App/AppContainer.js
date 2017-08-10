e2016263affda5235d2b79c7e6ac4acf69da6bc6
J Scott Smith <jscsmith@gmail.com>
2017-08-09 22:58:15 -0700

    add not about unused AppContainer

--- src/universal/containers/App/AppContainer.js
+++ src/universal/containers/App/AppContainer.js
@@ -1,6 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import Main from 'universal/components/organisms/Main';
 
+// Currently unused because is it was confusing
+// and not providing an additional functionality
+// Now using Main directly in Routes
+
 class AppContainer extends Component {
     static propTypes = {
         children: PropTypes.element.isRequired,
