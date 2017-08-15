389159ef2fe653834885e64723218e7e19fa1521
J Scott Smith <jscsmith@gmail.com>
2017-08-15 01:18:33 -0700

    add fixtures

---
+++ src/database/fixtures/projects.js
@@ -0,0 +1,9 @@
+const projects = [
+    require('./projects/david-and-goliath'),
+    require('./projects/ford-by-design'),
+    require('./projects/lg-lifes-good-when-you-play-more'),
+    require('./projects/put-your-profile-on-the-line'),
+    require('./projects/zillow-homeis'),
+];
+
+module.exports = projects;
---
+++ src/database/fixtures/projects/lg-lifes-good-when-you-play-more.js
@@ -0,0 +1,14 @@
+const project = {
+    title: "Life's Good When You Play More",
+    slug: 'lg-lifes-good-when-you-play-more',
+    agency: 'BBDO',
+    role: 'Front-end Developer',
+    site_url: 'http://www.lg.com/global/g5-play-more',
+    description: "An interactive <strong>microsite</strong> lets you play strange games and watch cat video to kickoff LG's latest phone, the LG G5.",
+    color_primary: '#8fe87a',
+    content: "Life's Good When You Play More",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+};
+
+module.exports = project;
