b7a494ea9ab29eb213d0f982e7c4ccb0f9c7a49e
J Scott Smith <jscsmith@gmail.com>
2017-08-19 01:58:15 -0700

    don’t use counter reducer

--- src/universal/redux/reducers/index.js
+++ src/universal/redux/reducers/index.js
@@ -1,2 +1,2 @@
-export { default as counter } from 'Universal/modules/counter/ducks/counter';
+// export { default as counter } from 'Universal/modules/counter/ducks/counter';
 export { default as projects } from 'Universal/modules/projects/projects';
