440711c403471705a440e93a59d0d45286c94411
J Scott Smith <jscsmith@gmail.com>
2017-08-19 01:57:27 -0700

    disable SSL in sequelize config on dev, remove test config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,30 @@
 const dotenv = require('dotenv').config();
 
+const PROD = process.env.NODE_ENV === 'production';
+
+// SSL is disabled on Local Dev
+
 module.exports = {
     development: {
+        // username: 'user',
+        // password: 'password',
+        // database: 'db',
+        // host: 'db',
+        // dialect: 'postgres',
+        // dialectOptions: {
+        //     ssl: false,
+        // },
         use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         dialectOptions: {
-            ssl: true,
-        },
-    },
-    test: {
-        use_env_variable: 'DATABASE_URL',
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: true,
+            ssl: PROD,
         },
     },
     production: {
         use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         dialectOptions: {
-            ssl: true,
+            ssl: PROD,
         },
     },
 };
