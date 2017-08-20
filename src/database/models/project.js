68f8273d7c5f62dac3fc40e2729871729254ff53
J Scott Smith <jscsmith@gmail.com>
2017-08-19 19:19:40 -0700

    update projects model

--- src/database/models/project.js
+++ src/database/models/project.js
@@ -3,6 +3,12 @@ module.exports = function(sequelize, DataTypes) {
     const Project = sequelize.define(
         'Project',
         {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
             title: {
                 type: DataTypes.STRING,
                 allowNull: false,
@@ -11,14 +17,38 @@ module.exports = function(sequelize, DataTypes) {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
-        },
-        {
-            classMethods: {
-                associate(models) {
-                    // associations can be defined here
-                },
+            slug: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            agency: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            site_url: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            color_primary: {
+                type: DataTypes.STRING,
+                allowNull: false,
             },
         }
+        // {
+        //     classMethods: {
+        //         associate(models) {
+        //             // associations can be defined here
+        //         },
+        //     },
+        // }
     );
     return Project;
 };
