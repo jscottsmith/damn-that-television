8f3bd656c5829935bc600b9a722bb8d63f09401b
J Scott Smith <jscsmith@gmail.com>
2017-08-22 01:01:30 -0700

    change more universal aliases

--- src/universal/routes/static.js
+++ src/universal/routes/static.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Home from 'Universal/components/pages/Home.js';
-import NotFound from 'Universal/components/pages/NotFound.js';
-import ProjectsContainer from 'Universal/modules/projects/ProjectsContainer';
-import ProjectContainer from 'Universal/modules/project/ProjectContainer';
-import Main from 'Universal/components/organisms/Main';
+import Home from 'universal/components/pages/Home.js';
+import NotFound from 'universal/components/pages/NotFound.js';
+import ProjectsContainer from 'universal/modules/projects/ProjectsContainer';
+import ProjectContainer from 'universal/modules/project/ProjectContainer';
+import Main from 'universal/components/organisms/Main';
 
 const routes = [
     {
