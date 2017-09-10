63d0ab58b0cc8bd070923ea1dd56fbe7f697b08a
J Scott Smith <jscsmith@gmail.com>
2017-09-10 15:18:59 -0700

    add home content

--- src/universal/components/organisms/Intro.js
+++ src/universal/components/organisms/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Copy from '../atoms/Copy';
+import Copy from 'Atoms/Copy';
 
 export default function Intro(props) {
     const { description } = props;
--- src/universal/components/pages/Home.js
+++ src/universal/components/pages/Home.js
@@ -1,10 +1,15 @@
 import React, { PureComponent } from 'react';
+// import Damnit from 'Organisms/Damnit';
+import cx from 'classnames';
+import styles from 'Styles/pages/_index.scss';
+import Copy from 'Atoms/Copy';
+import content from 'content/home/home.md';
 
 export default class Home extends PureComponent {
     render() {
         return (
-            <article className="page-index">
-                <h1>Home</h1>
+            <article className={cx('page-index', styles.index)}>
+                <Copy className={styles.welcome}>{content}</Copy>
             </article>
         );
     }
