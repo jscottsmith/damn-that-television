66f12de158a48c420c9fb4d6936aa9a35045e26b
J Scott Smith <jscsmith@gmail.com>
2017-09-21 23:29:21 -0700

    just go

--- src/universal/components/pages/Home.js
+++ src/universal/components/pages/Home.js
@@ -28,7 +28,7 @@ export default class Home extends PureComponent {
                 >
                     <Copy>{content}</Copy>
                     <button className={styles.play} onClick={this.handlePlay}>
-                        📺 Go! 👊🏻
+                        Go!
                     </button>
                 </div>
                 <Damnit
