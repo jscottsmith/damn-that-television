11cac37afa2cd0e3b3f3b8ce52ae572e5129bad2
J Smith <jscsmith@gmail.com>
2020-08-09 23:18:36 -0700

    move to index file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
-import Fade from '../../components/Fade';
-import IntroLanding from '../../components/sections/IntroLanding/IntroLanding';
+import Fade from 'components/Fade';
+import IntroLanding from 'components/sections/IntroLanding/IntroLanding';
 
 export default class Home extends PureComponent {
     render() {
