e4dc5b61b3f99a878e6375ad6c1a737f46b43098
J Scott Smith <jscsmith@gmail.com>
2022-01-02 14:43:13 -0800

    remove getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,6 @@ import { TransitionGroup } from 'react-transition-group';
 import '../sass/root.scss';
 import './resume/Resume.scss';
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
