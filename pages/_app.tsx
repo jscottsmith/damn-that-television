737a978619110af57c75620749cb6775bfbeae12
J Scott Smith <jscsmith@gmail.com>
2022-01-27 21:15:40 -0800

    feat: add canonical url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,32 @@
-import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { useRouter } from 'next/router';
 import { TransitionGroup } from 'react-transition-group';
 import '../sass/root.scss';
 import '../sass/globals.scss';
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
-          <link
-            rel="stylesheet"
-            href="https://use.typekit.net/bwo5nqc.css"
-          ></link>
-        </Head>
-        <TransitionGroup>
-          <Component {...pageProps} />
-        </TransitionGroup>
-      </>
-    );
-  }
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/bwo5nqc.css"
+        ></link>
+        <link
+          rel="canonical"
+          href={`https://damnthat.tv${router.pathname}`}
+        ></link>
+      </Head>
+      <TransitionGroup>
+        <Component {...pageProps} />
+      </TransitionGroup>
+    </>
+  );
 }
+export default MyApp;
