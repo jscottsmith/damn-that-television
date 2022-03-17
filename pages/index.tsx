691d8a870fb6b248aaa40dfd6b6cb5d4ea22b12d
J Scott Smith <jscsmith@gmail.com>
2022-03-17 10:42:51 -0700

    feat: prismic homepage content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { withRouter } from 'next/router';
 import Fade from 'components/Fade';
 import IntroLanding from 'components/sections/IntroLanding/IntroLanding';
+import Client from 'utils/prismicHelpers';
 
-export default class Home extends PureComponent {
-  render() {
-    return (
-      <Fade in>
-        <IntroLanding />
-      </Fade>
-    );
-  }
+export async function getStaticProps({ params }) {
+  const document = await Client().getSingle('homepage', {});
+
+  return {
+    props: {
+      document,
+    },
+  };
 }
+
+const Page = (props) => {
+  return (
+    <Fade in>
+      <IntroLanding document={props.document} />
+    </Fade>
+  );
+};
+
+export default withRouter(Page);
