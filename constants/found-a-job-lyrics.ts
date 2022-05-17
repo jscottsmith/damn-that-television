c3ed99bf479cb6e7c1c99656138bc4ac885ab9e7
J Scott Smith <jscsmith@gmail.com>
2022-05-16 22:07:35 -0700

    feat: marquee lyrics

---
+++ constants/found-a-job-lyrics.ts
@@ -0,0 +1,42 @@
+export const FOUND_A_JOB_LYRICS = [
+  'Damn that television, what a bad picture',
+  "Don't get upset, it's not a major disaster",
+  "'There's nothing on tonight,' he said, 'I don't know what's the matter'",
+  "'Nothing's ever on,' she said, 'so I don't know why you bother'",
+  `We've heard this little scene, we've heard it many times`,
+  `People fighting over little things and wasting precious time`,
+  `They might be better off, I think, the way it seems to me`,
+  `Making up their own shows, which might be better than TV`,
+
+  `Judy's in the bedroom, inventing situations`,
+  `Bob is on the street today, scouting up locations`,
+  `They've enlisted all their family`,
+  `They've enlisted all their friends`,
+  `It helped save their relationship`,
+  `And made it work again`,
+
+  `Their show gets real high ratings, they think they have a hit`,
+  `There might even be a spin-off, but they're not sure 'bout that`,
+  `If they ever watch TV again, it'd be too soon for them`,
+  `Bob never yells about the picture now, he's having too much fun`,
+
+  `Judy's in the bedroom, inventing situations`,
+  `Bob is on the street today, scouting up locations`,
+  `They've enlisted all their family`,
+  `They've enlisted all their friends`,
+  `It helped save their relationship`,
+  `And made it work again`,
+
+  `So think about this little scene, apply it to your life`,
+  `If your work isn't what you love, then something isn't right`,
+  `Just think of Bob and Judy, they're happy as can be`,
+  `Inventing situations, putting them on TV`,
+
+  `Judy's in the bedroom, inventing situations`,
+  `Bob is on the street today, he's having a vacation`,
+  `They've enlisted all their family`,
+  `They've enlisted all their friends`,
+  `It helped save their relationship`,
+  `And made it work again`,
+  `Hit it`,
+];
