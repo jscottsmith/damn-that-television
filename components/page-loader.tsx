f01a4902acc90702679063c29b25f1c467a13372
J Scott Smith <jscsmith@gmail.com>
2022-08-09 11:25:39 -0700

    chore: transition opacity on loader

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -1,9 +1,17 @@
 import { motion } from 'framer-motion';
+import cx from 'classnames';
+import { useHasMounted } from 'hooks/use-has-mounted';
 import Eye from './eye';
 
 export function PageLoader() {
+  const hasMounted = useHasMounted();
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-pepto text-deep">
+    <div
+      className={cx(
+        'fixed inset-0 flex items-center justify-center bg-pepto text-deep transition-opacity duration-500 ease-out',
+        { 'opacity-0': !hasMounted },
+      )}
+    >
       <div>
         <motion.div
           className="mb-4 flex justify-center"
