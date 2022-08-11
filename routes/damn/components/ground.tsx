28135bf832e8676d9841708a957a17ce36615116
J Scott Smith <jscsmith@gmail.com>
2022-08-10 18:02:54 -0700

    fix: ts errors

--- routes/damn/components/ground.tsx
+++ routes/damn/components/ground.tsx
@@ -1,7 +1,10 @@
 import { PlaneProps, usePlane } from '@react-three/cannon';
 
 export function Ground(props: PlaneProps) {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  const [ref] = usePlane<THREE.Mesh>(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    ...props,
+  }));
 
   return (
     <mesh ref={ref}>
