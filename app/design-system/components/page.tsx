3cd169b6865c5dc85ac9239610586813cf1c7451
J Scott Smith <jscsmith@gmail.com>
2023-11-04 23:28:08 -0700

    feat: custom bg color in card

--- app/design-system/components/page.tsx
+++ app/design-system/components/page.tsx
@@ -1,14 +1,21 @@
-import { Card } from '@/components/card';
+import { Card, CardMiami } from '@/components/card';
 import { SiteWrapper } from '@/components/site-wrapper';
 
 export default function Components() {
   return (
-    <SiteWrapper className="bg-pepto" padY>
-      <section className="grid gap-lg md:grid-cols-3 md:gap-xl lg:gap-2xl">
-        <Card className="h-48 p-base md:p-lg" />
-        <Card className="h-48 p-base md:p-lg" />
-        <Card className="h-48 p-base md:p-lg" />
-      </section>
-    </SiteWrapper>
+    <>
+      <SiteWrapper className="min-h-screen bg-fab-600" padY>
+        <section className="grid gap-lg md:grid-cols-3 md:gap-xl lg:gap-2xl">
+          <Card className="h-96 p-base md:p-lg" />
+          <Card className="h-96 p-base md:p-lg" />
+          <Card className="h-96 p-base md:p-lg" />
+        </section>
+      </SiteWrapper>
+      <SiteWrapper className="min-h-screen bg-ghost" padY>
+        <section className="grid grid-cols-1">
+          <CardMiami className="h-48 p-base md:p-lg" />
+        </section>
+      </SiteWrapper>
+    </>
   );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -1,16 +1,25 @@
 import React, { HTMLAttributes, PropsWithChildren } from 'react';
 import clsx from 'clsx';
 
+type CardProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>> & {
+  backgroundColor?: string;
+};
+
 export function Card({
+  backgroundColor = 'bg-ghost',
   className,
   ...props
-}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+}: CardProps) {
   return (
     <div
-      className={clsx(className, 'bg-ghost text-deep shadow-hard')}
+      className={clsx(className, 'text-deep shadow-hard', backgroundColor)}
       {...props}
     >
       {props.children}
     </div>
   );
 }
+
+export const CardGhost = Card;
+
+export const CardMiami = (props: CardProps) => <Card backgroundColor="bg-miami" {...props} />;
