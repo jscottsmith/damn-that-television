4d2672d8c80ca38c7dd4f84bfc40d124e52714d2
J Scott Smith <jscsmith@gmail.com>
2023-11-05 14:49:19 -0800

    feat: add next-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import { Providers } from './providers';
 
 import '/sass/root.scss';
 import '/sass/globals.scss';
@@ -7,7 +8,6 @@ export const metadata: Metadata = {
   title: 'Design System',
   metadataBase: new URL('https://damnthat.tv'),
   description: 'Damnthat.tv | Design System',
-  manifest: 'https://nextjs.org/manifest.json',
   icons: {
     apple: '/apple-touch-icon.png',
   },
@@ -19,17 +19,29 @@ export const viewport: Viewport = {
   minimumScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // NOTE: If you do not add suppressHydrationWarning to your <html> you
+  // will get warnings because next-themes updates that element. This property
+  // only applies one level deep, so it won't block hydration warnings on other elements.
+  // @see https://github.com/pacocoursey/next-themes#with-app
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/bwo5nqc.css"
+        ></link>
+      </head>
       <body>
-        <main>{children}</main>
+        <main>
+          <Providers>{children}</Providers>
+        </main>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './routes/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
