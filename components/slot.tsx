90ffcc393c2e957d623e55b7f98a5e40fa7098b3
J Scott Smith <jscsmith@gmail.com>
2023-11-05 16:12:47 -0800

    feat: add slot component

---
+++ components/slot.tsx
@@ -0,0 +1,32 @@
+import { twMerge } from 'tailwind-merge';
+import * as React from 'react';
+
+// Used instead of Polymorphic components
+// https://www.jacobparis.com/content/react-as-child
+
+export type AsChildProps<DefaultElementProps> =
+  | ({ asChild?: false } & DefaultElementProps)
+  | { asChild: true; children: React.ReactNode };
+
+export function Slot({
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLElement> & {
+  children?: React.ReactNode;
+}) {
+  if (React.isValidElement(children)) {
+    return React.cloneElement(children, {
+      ...props,
+      ...children.props,
+      style: {
+        ...props.style,
+        ...children.props.style,
+      },
+      className: twMerge(props.className, children.props.className),
+    });
+  }
+  if (React.Children.count(children) > 1) {
+    React.Children.only(null);
+  }
+  return null;
+}
