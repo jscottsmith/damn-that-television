3be008d9cfc3bcc1b8d570d18c78488c1c7826a0
J Scott Smith <jscsmith@gmail.com>
2023-12-02 10:45:39 -0800

    feat: add surface group

--- components/surface.tsx
+++ components/surface.tsx
@@ -4,6 +4,8 @@ import { SlotComponent, SlotComponentProps } from './slot';
 export const PRIMARY_SURFACE_CLASS = 'bg-white dark:bg-slate-900';
 export const SECONDARY_SURFACE_CLASS = 'bg-slate-100 dark:bg-slate-700';
 export const SURFACE_BACKGROUND_CLASS = 'bg-slate-200 dark:bg-slate-800';
+export const SURFACE_GROUP_CLASS =
+  'border border-slate-200 dark:border-slate-800';
 
 export function SurfacePrimary({ className, ...props }: SlotComponentProps) {
   return (
@@ -27,6 +29,15 @@ export function SurfaceBackground({ className, ...props }: SlotComponentProps) {
   return (
     <SlotComponent
       className={clsx(SURFACE_BACKGROUND_CLASS, className)}
+      {...props}
+    />
+  );
+}
+
+export function SurfaceGroup({ className, ...props }: SlotComponentProps) {
+  return (
+    <SlotComponent
+      className={clsx(SURFACE_GROUP_CLASS, className)}
       {...props}
     />
   );
