8936105186e845d2daac340814fe2e3249f12537
J Scott Smith <jscsmith@gmail.com>
2023-12-01 23:39:30 -0800

    chore: use prose on home page

--- app/home/components/introduction/index.tsx
+++ app/home/components/introduction/index.tsx
@@ -3,12 +3,15 @@ import clsx from 'clsx';
 import { RichText } from 'prismic-reactjs';
 import styles from './index.module.scss';
 import { CardSecondary } from '@/components/card';
+import { Prose } from '@/components/typography/prose';
 
 export const Introduction = (props) => {
   return (
     <div className={clsx(styles.welcome)}>
-      <CardSecondary className={clsx(styles.copy, 'p-md md:p-lg')}>
-        <RichText render={props.document?.data?.introduction} />
+      <CardSecondary className={clsx('p-md md:p-lg')}>
+        <Prose className="prose-lg lg:prose-xl xl:prose-2xl max-w-2xl">
+          <RichText render={props.document?.data?.introduction} />
+        </Prose>
       </CardSecondary>
     </div>
   );
