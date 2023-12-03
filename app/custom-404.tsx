425770bb1b9cb231d67a12e66df891f7bc1f99b1
J Scott Smith <jscsmith@gmail.com>
2023-12-03 14:58:06 -0800

    feat: custom 404

--- app/custom-404.tsx
+++ app/custom-404.tsx
@@ -1,17 +1,16 @@
+'use client';
 import { CanvasHero } from 'components/canvas-hero';
 import { Eraser } from 'canvas/eraser/Eraser';
 import { HeaderNav } from '@/routes/components/header-nav';
-import Link from 'next/link';
 
 const eraser = new Eraser();
 
-export default function Custom404() {
+export function Custom404() {
   return (
     <div className="bg-peach">
       <HeaderNav />
-
       <h1
-        className="text-lunar font-medium absolute inset-0 top-0 left-0 flex items-center justify-center select-none"
+        className="text-lunar font-futura font-medium absolute inset-0 top-0 left-0 flex items-center justify-center select-none"
         style={{ fontSize: '15vw' }}
       >
         404
---
+++ app/not-found.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Custom404 } from './custom-404';
+
+export default function NotFound() {
+  return <Custom404 />;
+}
