fb4e590e2d69557f425eb9f359009fe6ebf66e04
J Scott Smith <jscsmith@gmail.com>
2023-12-14 23:23:00 -0800

    chore: surface shadow shared

--- components/surface-interactive.tsx
+++ components/surface-interactive.tsx
@@ -16,32 +16,40 @@ export enum SurfaceInteractiveName {
 
 export type SurfaceInteractiveNames = keyof typeof SurfaceInteractiveName;
 
+const SHARED_SHADOW_COLOR = 'shadow-slate-700 dark:shadow-slate-200';
+
 export const SURFACE_INTERACTIVE = clsx(
   'bg-slate-200 hocus:bg-slate-300',
   'dark:bg-slate-900 dark:hocus:bg-slate-600',
+  SHARED_SHADOW_COLOR,
 );
 
 const SURFACE_INTERACTIVE_NAME_MAP = {
   [SurfaceInteractiveName.primary]: clsx(
     'text-white bg-slate-800 hocus:bg-club',
     'dark:text-slate-800 dark:bg-slate-200 dark:hocus:bg-club',
+    'shadow-club',
   ),
   [SurfaceInteractiveName.secondary]: SURFACE_INTERACTIVE,
   [SurfaceInteractiveName.warning]: clsx(
     'text-white bg-orange-500 hocus:bg-orange-600',
     'dark:text-orange-950 dark:hocus:bg-orange-400',
+    SHARED_SHADOW_COLOR,
   ),
   [SurfaceInteractiveName.danger]: clsx(
     'text-white bg-rose-500 hocus:bg-rose-600',
     'dark:text-rose-950 dark:hocus:bg-rose-400',
+    SHARED_SHADOW_COLOR,
   ),
   [SurfaceInteractiveName.info]: clsx(
     'text-white bg-sky-500 hocus:bg-sky-600',
     'dark:text-sky-950 dark:hocus:bg-sky-400',
+    SHARED_SHADOW_COLOR,
   ),
   [SurfaceInteractiveName.success]: clsx(
     'text-white bg-teal-500 hocus:bg-teal-600',
     'dark:text-teal-950 dark:hocus:bg-teal-400',
+    SHARED_SHADOW_COLOR,
   ),
 };
 
@@ -64,8 +72,7 @@ export function SurfaceInteractive({
         mapNameToClassName(name),
         className,
         'items-center inline-flex rounded-full',
-        // color
-        'shadow-slate-700 dark:shadow-slate-950',
+
         // hover
         'hover:shadow-hard-xs hover:-translate-x-0.5 hover:-translate-y-0.5',
         // focus
