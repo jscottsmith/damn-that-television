bda6ec4377f587a8e321692ff7c2ba2ea7c8a398
J Scott Smith <jscsmith@gmail.com>
2025-03-14 23:00:35 -0700

    fix: bounding rect hook

--- hooks/use-client-bounding-rect.ts
+++ hooks/use-client-bounding-rect.ts
@@ -1,18 +1,20 @@
-import { RefObject, useEffect, useState } from 'react';
+import { RefObject, useCallback, useEffect, useState } from 'react';
 import { useDebounceCallback, useEventListener } from 'usehooks-ts';
 
 export function useBoundingClientRect(ref: RefObject<HTMLDivElement | null>) {
   const [rect, setRect] = useState<DOMRect | null>(null);
 
-  function updateRect() {
-    setRect(ref.current!.getBoundingClientRect());
-  }
+  const updateRect = useCallback(() => {
+    if (ref.current) {
+      setRect(ref.current.getBoundingClientRect());
+    }
+  }, [ref]);
 
   const debouncedUpdateRect = useDebounceCallback(updateRect, 200);
 
   useEffect(() => {
-    setRect(ref.current!.getBoundingClientRect());
-  }, [ref]);
+    updateRect();
+  }, [ref, updateRect]);
 
   useEventListener('resize', debouncedUpdateRect);
   useEventListener('scroll', debouncedUpdateRect);
