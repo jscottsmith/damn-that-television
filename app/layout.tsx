9b4e300d5af41f4b33c896687cc15fcd1b86b0c4
J Scott Smith <jscsmith@gmail.com>
2025-03-16 14:38:50 -0700

    fix: use next font for poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata, Viewport } from 'next';
 import { Providers } from './providers';
-
-import '/sass/root.scss';
-import '/sass/globals.scss';
-import { HeaderNav } from '@/components/page-globals/header-nav';
+import { Poppins } from 'next/font/google';
 import Script from 'next/script';
+import '/sass/root.scss';
+import '/sass/globals.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Design System',
@@ -33,7 +38,7 @@ export default function RootLayout({
   // only applies one level deep, so it won't block hydration warnings on other elements.
   // @see https://github.com/pacocoursey/next-themes#with-app
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={poppins.className} suppressHydrationWarning>
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/bwo5nqc.css" />
         <link rel="manifest" href="/site.webmanifest" />
