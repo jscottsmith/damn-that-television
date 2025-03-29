9e31b696f699aaf7579968af0fd51580d3354b1c
J Scott Smith <jscsmith@gmail.com>
2025-03-28 22:35:59 -0700

    feat: embla dots

--- hooks/use-cycle-index.ts
+++ hooks/use-cycle-index.ts
@@ -1,13 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from 'react';
+
+export type UseCycleIndex = ReturnType<typeof useCycleIndex>;
+
+enum Directions {
+  forward = 'forward',
+  backward = 'backward',
+}
+
+type Direction = keyof typeof Directions;
 
 export function useCycleIndex(length: number) {
   const [index, setIndex] = useState(0);
-  const next = () => setIndex((index + 1) % length);
-  const previous = () => setIndex((index - 1 + length) % length);
+  const prevIndex = useRef(index);
+  const direction = useRef<Direction>(null);
+
+  function updateIndex(nextIndex) {
+    prevIndex.current = index;
+    setIndex(nextIndex);
+  }
+  function next() {
+    updateIndex((index + 1) % length);
+    direction.current = Directions.forward;
+  }
+  function previous() {
+    updateIndex((index - 1 + length) % length);
+    direction.current = Directions.backward;
+  }
+  function goTo(i) {
+    updateIndex(i);
+  }
 
   return {
+    prevIndex: prevIndex.current,
     index,
     next,
     previous,
+    goTo,
   };
 }
