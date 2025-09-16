abed9cf5e175f99d04cd14f0c318136b4afdce2a
J Scott Smith <jscsmith@gmail.com>
2025-09-16 14:56:59 -0700

    feat: add command overlay

--- components/anchor-link-copy.tsx
+++ components/anchor-link-copy.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import clsx from 'clsx';
 import { LinkIcon, CheckCircleIcon } from '@heroicons/react/24/solid';
 import { useCopyToClipboard, useTimeout } from 'usehooks-ts';
+import { CommandOverlay } from './command-overlay';
 
 interface AnchorLinkCopyProps {
   id: string;
@@ -17,7 +18,7 @@ export const AnchorLinkCopy = (props: AnchorLinkCopyProps) => {
     () => {
       setIsCopied(false);
     },
-    isCopied ? 3000 : null,
+    isCopied ? 1500 : null,
   );
 
   const handleCopyLink = () => {
@@ -27,29 +28,32 @@ export const AnchorLinkCopy = (props: AnchorLinkCopyProps) => {
   };
 
   return (
-    <a
-      href={`#${props.id}`}
-      onClick={handleCopyLink}
-      className={clsx(
-        'group relative -m-1 block rounded-md p-1 text-xs transition-colors',
-        'hover:bg-slate-100 dark:hover:bg-slate-700',
-        props.className,
-      )}
-      aria-label="Copy link to this section"
-    >
-      {props.children}
-      <div
+    <>
+      <a
+        href={`#${props.id}`}
+        onClick={handleCopyLink}
         className={clsx(
-          'absolute right-2 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100',
-          isCopied && 'opacity-100',
+          'group relative -m-1 block rounded-md p-1 text-xs transition-colors',
+          'hover:bg-slate-100 dark:hover:bg-slate-700',
+          props.className,
         )}
+        aria-label="Copy link to this section"
       >
-        {isCopied ? (
-          <CheckCircleIcon className="h-5 w-5 text-miami-500" />
-        ) : (
-          <LinkIcon className="h-4 w-4" />
-        )}
-      </div>
-    </a>
+        {props.children}
+        <div
+          className={clsx(
+            'absolute right-2 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100',
+            isCopied && 'opacity-100',
+          )}
+        >
+          {isCopied ? (
+            <CheckCircleIcon className="h-5 w-5 text-miami-500" />
+          ) : (
+            <LinkIcon className="h-4 w-4" />
+          )}
+        </div>
+      </a>
+      <CommandOverlay>{isCopied && 'URL Copied'}</CommandOverlay>
+    </>
   );
 };
