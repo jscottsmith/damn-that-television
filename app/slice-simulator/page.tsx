587016468925142a2351cd7de33a6c42162a29aa
J Scott Smith <jscsmith@gmail.com>
2025-09-17 14:28:35 -0700

    feat: upgrade legacy slices

---
+++ app/slice-simulator/page.tsx
@@ -0,0 +1,21 @@
+import {
+  SliceSimulator,
+  SliceSimulatorParams,
+  getSlices,
+} from '@slicemachine/adapter-next/simulator';
+import { SliceZone } from '@prismicio/react';
+
+import { components } from '../../slices';
+
+export default async function SliceSimulatorPage({
+  searchParams,
+}: SliceSimulatorParams) {
+  const { state } = await searchParams;
+  const slices = getSlices(state);
+
+  return (
+    <SliceSimulator>
+      <SliceZone slices={slices} components={components} />
+    </SliceSimulator>
+  );
+}
---
+++ prismicio.ts
@@ -0,0 +1,46 @@
+import {
+  createClient as baseCreateClient,
+  type ClientConfig,
+  type Route,
+} from '@prismicio/client';
+import { enableAutoPreviews } from '@prismicio/next';
+import sm from './slicemachine.config.json';
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName =
+  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || sm.repositoryName;
+
+/**
+ * A list of Route Resolver objects that define how a document's `url` field is resolved.
+ *
+ * {@link https://prismic.io/docs/route-resolver#route-resolver}
+ */
+// TODO: Update the routes array to match your project's route structure.
+const routes: Route[] = [
+  // Examples:
+  // { type: "homepage", path: "/" },
+  // { type: "page", path: "/:uid" },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config - Configuration for the Prismic client.
+ */
+export const createClient = (config: ClientConfig = {}) => {
+  const client = baseCreateClient(repositoryName, {
+    routes,
+    fetchOptions:
+      process.env.NODE_ENV === 'production'
+        ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
+        : { next: { revalidate: 5 } },
+    ...config,
+  });
+
+  enableAutoPreviews({ client });
+
+  return client;
+};
