7f0458fedf5fa283dd100e272a80cc911ec49a65
J Scott Smith <jscsmith@gmail.com>
2025-10-15 09:39:02 -0700

    fix: overrflow from badge

--- app/(main)/home/components/recruiter-link/index.tsx
+++ app/(main)/home/components/recruiter-link/index.tsx
@@ -13,16 +13,19 @@ const variant = {
 
 export const RecruiterLink = () => {
   return (
-    <Link href={ROUTE_RESUME}>
-      <motion.div whileHover="hover" initial="rest">
-        <RecruiterBadge className="group absolute -right-2 -top-0 z-10 h-64 w-64 text-2xl md:h-96 md:w-96">
-          <motion.div variants={variant}>
-            <Magnetic>
-              <PointerFinger className="h-16 w-16 md:h-24 md:w-24" />
-            </Magnetic>
-          </motion.div>
-        </RecruiterBadge>
-      </motion.div>
-    </Link>
+    // fix overflow issue
+    <div className="pointer-events-none absolute inset-0 h-full w-full overflow-hidden">
+      <Link href={ROUTE_RESUME}>
+        <motion.div whileHover="hover" initial="rest">
+          <RecruiterBadge className="group absolute -right-2 -top-0 z-10 h-64 w-64 text-2xl md:h-96 md:w-96">
+            <motion.div variants={variant}>
+              <Magnetic>
+                <PointerFinger className="h-16 w-16 md:h-24 md:w-24" />
+              </Magnetic>
+            </motion.div>
+          </RecruiterBadge>
+        </motion.div>
+      </Link>
+    </div>
   );
 };
