0b1b8705561b7f36d9cce31303dd003881970441
J Scott Smith <jscsmith@gmail.com>
2026-07-12 22:19:03 -0700

    Drop custom spacing keys from tailwind-merge config

--- apps/damnthat.tv/helpers/tailwind-merge.ts
+++ apps/damnthat.tv/helpers/tailwind-merge.ts
@@ -4,26 +4,6 @@ import { extendTailwindMerge } from 'tailwind-merge';
 export const twMerge = extendTailwindMerge({
   extend: {
     theme: {
-      spacing: [
-        'sidemenu',
-        '2xs',
-        'xs',
-        'sm',
-        'base',
-        'md',
-        'lg',
-        'xl',
-        '2xl',
-        '3xl',
-        '4xl',
-        '5xl',
-        '6xl',
-        '7xl',
-        '8xl',
-        '10xl',
-        '11xl',
-        '12xl',
-      ],
       shadow: ['hard', 'hard-xs', 'hard-sm'],
     },
   },
